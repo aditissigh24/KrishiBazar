479d225f78ab2e3db1880f85a1a77bd1f40051c8
aditissigh24 <aditissigh24@gmail.com>
2025-04-15 00:29:50 +0530

    added order route

--- server/app.js
+++ server/app.js
@@ -9,11 +9,11 @@ app.use(cookieParser());
 // // Route Imports
 import product_route from "./routes/ProductsRoute.js";
 import user_route from "./routes/UserRoutes.js";
-// import order_route from "./routes/OrdersRoute.js";
+import order_route from "./routes/OrdersRoute.js";
 // this middleware will handle all the operations on products
 app.use("/products", product_route);
 app.use("/user", user_route);
-// app.use("/order", order_route);
+app.use("/order", order_route);
 // Middleware for Errors
 app.use(errorMiddleware);
 
