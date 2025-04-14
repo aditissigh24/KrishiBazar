4354fef921989c5e11f270987fe2e2254550a086
aditissigh24 <aditissigh24@gmail.com>
2025-04-14 22:10:38 +0530

    added product routes

--- server/app.js
+++ server/app.js
@@ -7,11 +7,11 @@ app.use(json());
 app.use(cookieParser());
 
 // // Route Imports
-// import product_route from "./routes/ProductsRoute.js";
+import product_route from "./routes/ProductsRoute.js";
 import user_route from "./routes/UserRoutes.js";
 // import order_route from "./routes/OrdersRoute.js";
 // this middleware will handle all the operations on products
-// app.use("/products", product_route);
+app.use("/products", product_route);
 app.use("/user", user_route);
 // app.use("/order", order_route);
 // Middleware for Errors
