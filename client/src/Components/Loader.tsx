67dba06c115d2be90cf6c4dedd10e87c3e5ef3a8
aditissigh24 <aditissigh24@gmail.com>
2025-04-21 03:39:31 +0530

    added loader

---
+++ client/src/Components/Loader.tsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="flex items-center justify-center flex-1 min-h-[70vh]">
+      <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-b-4 border-[#9a3b3b]" />
+    </div>
+  );
+};
+
+export default Loader;
