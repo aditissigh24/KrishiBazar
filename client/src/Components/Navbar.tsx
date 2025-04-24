af49511a857cab256c3ea661e690fa164f04b5e8
aditissigh24 <aditissigh24@gmail.com>
2025-04-24 18:20:46 +0530

    changed icons

--- client/src/Components/Navbar.tsx
+++ client/src/Components/Navbar.tsx
@@ -86,32 +86,20 @@ const Navbar = () => {
           </div>
           <div className="flex items-center space-x-4">
             {/* Cart Icon */}
-            <Link to="/cart" className="text-gray-700">
-              <svg
-                className="h-6 w-6"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
-              </svg>
+            <Link
+              to="/cart"
+              className="text-gray-700 hover:text-white relative"
+            >
+              <GiBeachBag className="w-8 h-8 object-cover" />
+              {totalItems > 0 && (
+                <span className="absolute -top-2 -right-2 bg-[#176112] text-white w-5 h-5 rounded-full flex items-center justify-center text-xs">
+                  {totalItems}
+                </span>
+              )}
             </Link>
             {/* User Profile Icon */}
-            <Link to="/profile" className="text-gray-700">
-              <svg
-                className="h-6 w-6"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
-              </svg>
+            <Link to="/profile" className="text-gray-700 hover:text-white">
+              <BsFillPersonFill className="w-8 h-8" />
             </Link>
             {/* Mobile Menu Button */}
             <button
